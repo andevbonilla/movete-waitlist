29fb1aedf001c38fb6907e563697619b44949927
Andres Bonilla <andresbonilla1256@gmail.com>
2024-07-31 15:20:03 -0500

    all translation config made

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import createMiddleware from 'next-intl/middleware';
+ 
+export default createMiddleware({
+  // A list of all locales that are supported
+  locales: ['en', 'es', 'ru', 'zh', 'fr', 'zh', 'hi', 'ja'],
+ 
+  // Used when no locale matches
+  defaultLocale: 'en'
+});
+ 
+export const config = {
+  // Match only internationalized pathnames
+  matcher: ['/', '/(en|es|ru|zh|fr|zh|hi|ja)/:path*']
+};
