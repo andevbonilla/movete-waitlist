764d0d561e6d47632d52aecfcf319e4754fb55f0
Andres Bonilla <andresbonilla1256@gmail.com>
2024-08-01 01:45:17 -0500

    bug of colors solved

--- src/components/ChangeLanguage.tsx
+++ src/components/ChangeLanguage.tsx
@@ -25,33 +25,57 @@ export const ChangeLanguage = ({ title }: { title: any }) => {
                 </button>
             </div>
             {
-                openLanguageMenu && <div className='z-[997] text-black fixed w-full top-0 bottom-0 left-0 bg-black bg-opacity-25 flex justify-center items-center'>
-                    <div onClick={() => { setopenLanguageMenu(false) }} className='cursor-pointer z-[998] fixed w-full top-0 bottom-0 left-0'></div>
+                openLanguageMenu && <div className='z-[997] text-black fixed w-full top-0 bottom-0 left-0 bg-black bg-opacity-45 flex justify-center items-center'>
+                    <div onClick={() => { setopenLanguageMenu(false) }} className='cursor-pointer z-[998] fixed w-full top-0 bottom-0 left-0 bg-transparent'></div>
                     <div className='bg-white p-10 rounded z-[999] overflow-y-scroll lg:overflow-hidden max-h-[80vh]'>
-                        <h3 className='font-bold mb-8 text-xl'>{title}</h3>
-                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4'>
-                            <Link href={currentUrl.replace(params.locale, "en")} className={`${(location.pathname.startsWith("/en")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                        <h3 className='font-bold mb-8 text-xl bg-white'>{title}</h3>
+                        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 bg-white'>
+                            <Link
+                                href={currentUrl.replace(params.locale, "en")}
+                                className={`${(location.pathname.startsWith("/en")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 English
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "es")} className={`${(location.pathname.startsWith("/es")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "es")}
+                                className={`${(location.pathname.startsWith("/es")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 Español
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "fr")} className={`${(location.pathname.startsWith("/fr")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "fr")}
+                                className={`${(location.pathname.startsWith("/fr")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 Français
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "zh")} className={`${(location.pathname.startsWith("/zh")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "zh")}
+                                className={`${(location.pathname.startsWith("/zh")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 中文繁體
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "ja")} className={`${(location.pathname.startsWith("/ja")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "ja")}
+                                className={`${(location.pathname.startsWith("/ja")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 日本語
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "pt")} className={`${(location.pathname.startsWith("/pt")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "pt")}
+                                className={`${(location.pathname.startsWith("/pt")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 Português
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "hi")} className={`${(location.pathname.startsWith("/hi")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "hi")}
+                                className={`${(location.pathname.startsWith("/hi")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 हिंदी
                             </Link>
-                            <Link href={currentUrl.replace(params.locale, "ru")} className={`${(location.pathname.startsWith("/ru")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded`}>
+                            <Link
+                                href={currentUrl.replace(params.locale, "ru")}
+                                className={`${(location.pathname.startsWith("/ru")) ? "bg-slate-300" : ""} hover:bg-slate-300 transition-all p-4 rounded bg-white`}
+                            >
                                 Русский
                             </Link>
                         </div>
